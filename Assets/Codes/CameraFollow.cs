3241c6adb6816095a2a6e98ef60dc3da9076164a
Useong <104291987+CodenameUS@users.noreply.github.com>
2024-05-20 00:31:37 +0900

    카메라 움직임 구현

--- Assets/Codes/CameraFollow.cs
+++ Assets/Codes/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     public Vector3 offset;
 
-    
+    // .. 카메라 조정
     void Update()
     {
         transform.position = target.position + offset;
