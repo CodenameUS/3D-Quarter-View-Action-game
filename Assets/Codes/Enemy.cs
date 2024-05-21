fc51b3d57500aec66abdeb179d475c0581992658
Useong <104291987+CodenameUS@users.noreply.github.com>
2024-05-21 17:35:43 +0900

    간단한 적 구현

--- Assets/Codes/Enemy.cs
+++ Assets/Codes/Enemy.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int maxHealth;
+    public int curHealth;
+
+    Material material;
+    Rigidbody rigid;
+    BoxCollider boxCollider;
+
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        material = GetComponent<MeshRenderer>().material;
     }
 
-    // Update is called once per frame
-    void Update()
+    // .. 충돌 이벤트
+    void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Melee")
+        {
+            Weapon weapon = other.GetComponent<Weapon>();
+            curHealth -= weapon.damage;
+            Vector3 reactVec = transform.position - other.transform.position;
+            StartCoroutine(OnDamaged(reactVec));
+        }
+        else if(other.tag == "Bullet")
+        {
+            Bullet bullet = other.GetComponent<Bullet>();
+            curHealth -= bullet.damage;
+            Vector3 reactVec = transform.position - other.transform.position;
+
+            Destroy(other.gameObject);
+            StartCoroutine(OnDamaged(reactVec));
+        }
+    }
+
+    // .. 피격 이벤트
+    IEnumerator OnDamaged(Vector3 reactVec)
+    {
+        material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+
+        if(curHealth > 0)
+        {
+            material.color = Color.white;
+        }
+        else
+        {
+            material.color = Color.gray;
+            gameObject.layer = 12;
+
+            reactVec = reactVec.normalized;
+            reactVec += Vector3.up;
+
+            rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+
+            Destroy(gameObject, 4);
+        }
     }
 }
